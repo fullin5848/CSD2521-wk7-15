da93bd3d94aff3025ebc9347ac9f20779779086c
Nathaniel Fullington <59833517+fullin5848@users.noreply.github.com>
2020-02-28 22:18:51 -0500

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 8

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
- * Created on 8/3/18 by AJT
+ * Updated on 2/28
+ NF
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
